e19fee3cede15956978ccb0040278d686d6ed794
Caro Meysmans <caro.meysmans@gmail.com>
2019-05-09 21:19:09 +0200

    Add folder structure

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# fpga-dsp
+# FPGA DSP
+
+## Naming convention
+
+### Verilog and VHDL
+
+### C and C++
+
+### Folders
+
+| Name  | Description                        |
+| ----- | ---------------------------------- |
+| ./hdl | Verilog and VHDL sources           |
+| ./inc | C and C++ headers                  |
+| ./ip  | Intellectual property              |
+| ./src | C and C++ sources                  |
+| ./tb  | Verilog and VHDL testbench sources |
+| ./xdc | Constraints                        |
---
+++ hdl/i2s_tx.v
@@ -0,0 +1,37 @@
+module i2s_tx #(
+    parameter pdata_width = 32
+) (
+    input wire rst,
+
+    input wire sclk_in,
+    input wire lrck_in,
+
+    input wire [pdata_width - 1 : 0] pldata_in,
+    input wire [pdata_width - 1 : 0] prdata_in,
+
+    output reg sdata_out
+);
+
+    // LRCK delayed by 1 SCLK cyle
+    reg lrck_d1_int;
+    // LRCK delayed by 2 SCLK cycles
+    reg lrck_d2_int;
+
+    always @(posedge sclk_in)
+        begin
+            if (rst)
+                begin
+                    lrck_d1_int <= 1'b0;
+                    lrck_d2_int <= 1'b0;
+                end
+            else
+                begin
+                    lrck_d1_int <= lrck_in;
+                    lrck_d2_int <= lrck_d1_int;
+                end
+        end
+
+    // LRCK pulse
+    wire lrck_p_int = lrck_d1_int ^ lrck_d2_int;
+
+endmodule
