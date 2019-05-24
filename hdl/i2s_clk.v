46e0779f75ee89e24941e559cf1593dea80362a5
Caro Meysmans <caro.meysmans@gmail.com>
2019-05-24 21:33:46 +0200

    Refactor i2s_clk

--- hdl/i2s_clk.v
+++ hdl/i2s_clk.v
@@ -13,28 +13,34 @@ module i2s_clk #(
     // MCLK_DIV_LRCK is maximally 2 ** 10
     reg [10 : 0] lrck_cnt;
 
-    always @(posedge mclk, negedge arstn)
-        if (arstn == 1'b0)
+    always @(posedge mclk, negedge arstn) begin
+        if (arstn == 1'b0) begin
             lrck_cnt <= 0;
-        else
-            if (lrck_cnt == MCLK_DIV_LRCK - 1)
+        end else begin
+            if (lrck_cnt == MCLK_DIV_LRCK - 1) begin
                 lrck_cnt <= 0;
-            else
+            end else begin
                 lrck_cnt <= lrck_cnt + 1;
+            end
+        end
+    end
 
     assign lrck = (lrck_cnt < MCLK_DIV_LRCK / 2) ? 1'b0 : 1'b1;
 
     // MCLK_DIV_SCLK is maximally 2 ** 10
     reg [10 : 0] sclk_cnt;
 
-    always @(posedge mclk, negedge arstn)
-        if (arstn == 1'b0)
+    always @(posedge mclk, negedge arstn) begin
+        if (arstn == 1'b0) begin
             sclk_cnt <= 0;
-        else
-            if (sclk_cnt == MCLK_DIV_SCLK - 1)
+        end else begin
+            if (sclk_cnt == MCLK_DIV_SCLK - 1) begin
                 sclk_cnt <= 0;
-            else
+            end else begin
                 sclk_cnt <= sclk_cnt + 1;
+            end
+        end
+    end
 
     assign sclk = (sclk_cnt < MCLK_DIV_SCLK / 2) ? 1'b0 : 1'b1;
 
